ce0c60f0798c015ac13763cc338a8df6eea96200
ankita kulkarni <kulkarni.ankita09@gmail.com>
2022-07-25 16:52:41 -0400

    Card Component Size

--- components/card/card.js
+++ components/card/card.js
@@ -1,11 +1,24 @@
 import Image from "next/image";
+import styles from "./card.module.css";
 
 const Card = (props) => {
   const { imgUrl, size } = props;
+  const classMap = {
+    large: styles.lgItem,
+    medium: styles.mdItem,
+    small: styles.smItem,
+  };
   return (
-    <div>
+    <div className={styles.container}>
       Card
-      <Image src={imgUrl} alt="image" width="300px" height="300px" />
+      <div className={classMap[size]}>
+        <Image
+          src={imgUrl}
+          alt="image"
+          layout="fill"
+          className={styles.cardImg}
+        />
+      </div>
     </div>
   );
 };
