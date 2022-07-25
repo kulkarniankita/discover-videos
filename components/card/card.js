8b345c0e582ac61fafeddf4d5f93e56e5341f40b
ankita kulkarni <kulkarni.ankita09@gmail.com>
2022-07-25 16:56:24 -0400

    Card Component Error Handling

--- components/card/card.js
+++ components/card/card.js
@@ -1,21 +1,31 @@
+import { useState } from "react";
 import Image from "next/image";
 import styles from "./card.module.css";
 
 const Card = (props) => {
-  const { imgUrl, size } = props;
+  const { imgUrl = "/static/clifford.webp", size = "medium" } = props;
+
+  const [imgSrc, setImgSrc] = useState(imgUrl);
   const classMap = {
     large: styles.lgItem,
     medium: styles.mdItem,
     small: styles.smItem,
   };
+
+  const handleOnError = () => {
+    console.log("hii error");
+    setImgSrc("/static/clifford.webp");
+  };
+
   return (
     <div className={styles.container}>
       Card
       <div className={classMap[size]}>
         <Image
-          src={imgUrl}
+          src={imgSrc}
           alt="image"
           layout="fill"
+          onError={handleOnError}
           className={styles.cardImg}
         />
       </div>
