caea4099c033fd38708b16cbdfb23d849776e519
ankita kulkarni <kulkarni.ankita09@gmail.com>
2022-07-25 17:16:00 -0400

    Section Cards Structure

---
+++ components/card/section-cards.js
@@ -0,0 +1,16 @@
+import Card from "./card";
+import styles from "./section-cards.module.css";
+
+const SectionCards = (props) => {
+  const { title } = props;
+  return (
+    <section className={styles.container}>
+      <h2 className={styles.title}>{title}</h2>
+      <div className={styles.cardWrapper}>
+        <Card imgUrl="/static/clifford.webp" size="large" />
+      </div>
+    </section>
+  );
+};
+
+export default SectionCards;
--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import styles from "../styles/Home.module.css";
 import Banner from "../components/banner/banner";
 import NavBar from "../components/nav/navbar";
 import Card from "../components/card/card";
+import SectionCards from "../components/card/section-cards";
 
 export default function Home() {
   return (
@@ -19,6 +20,8 @@ export default function Home() {
         subTitle="a very cute dog"
         imgUrl="/static/clifford.webp"
       />
+
+      <SectionCards title="Disney" />
 
       <Card imgUrl="/static/clifford.webp" size="large" />
       <Card size="medium" />
