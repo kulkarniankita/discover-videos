1032b208bd7aa66116c6f3a2c475e4518061f1f1
ankita kulkarni <kulkarni.ankita09@gmail.com>
2022-07-25 15:57:37 -0400

    NavBar Component Styling

--- components/nav/navbar.js
+++ components/nav/navbar.js
@@ -1,24 +1,32 @@
+import styles from "./navbar.module.css";
+
 const NavBar = (props) => {
   const { username } = props;
   return (
-    <div>
-      NavBar
-      <p>{username}</p>
-      <ul>
-        <li>Home</li>
-        <li>My List</li>
-      </ul>
-      <nav>
-        <div>
-          <button>
-            <p>{username}</p>
-          </button>
-
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <a className={styles.logoLink}>
+          <div className={styles.logoWrapper}>Netflix</div>
+        </a>
+        <ul className={styles.navItems}>
+          <li className={styles.navItem}>Home</li>
+          <li className={styles.navItem2}>My List</li>
+        </ul>
+        <nav className={styles.navContainer}>
           <div>
-            <a>Sign out</a>
+            <button className={styles.usernameBtn}>
+              <p className={styles.username}>{username}</p>
+            </button>
+
+            <div className={styles.navDropdown}>
+              <div>
+                <a className={styles.linkName}>Sign out</a>
+                <div className={styles.lineWrapper}></div>
+              </div>
+            </div>
           </div>
-        </div>
-      </nav>
+        </nav>
+      </div>
     </div>
   );
 };
