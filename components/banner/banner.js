9b01e0adbc6cbac5efafe679d5bc1f6a45d10a4b
ankita kulkarni <kulkarni.ankita09@gmail.com>
2022-07-25 15:22:52 -0400

    Banner Component Button With Font

--- components/banner/banner.js
+++ components/banner/banner.js
@@ -1,22 +1,34 @@
+import styles from "./banner.module.css";
+
 const Banner = (props) => {
   const { title, subTitle, imgUrl } = props;
 
   const handleOnPlay = () => {
     console.log("handleOnPlay");
   };
+
   return (
-    <div>
-      <h3>{title}</h3>
-      <h3>{subTitle}</h3>
-      <button onClick={handleOnPlay}>Play</button>
+    <div className={styles.container}>
+      <div className={styles.leftWrapper}>
+        <div className={styles.left}>
+          <div className={styles.nseriesWrapper}>
+            <p className={styles.firstLetter}>N</p>
+            <p className={styles.series}>S E R I E S</p>
+          </div>
+          <h3 className={styles.title}>{title}</h3>
+          <h3 className={styles.subTitle}>{subTitle}</h3>
+
+          <div className={styles.playBtnWrapper}>
+            <button className={styles.btnWithIcon} onClick={handleOnPlay}>
+              <span className={styles.playText}>Play</span>
+            </button>
+          </div>
+        </div>
+      </div>
       <div
+        className={styles.bannerImg}
         style={{
-          backgroundImage: `url(${imgUrl})`,
-          width: "100%",
-          height: "100%",
-          position: "absolute",
-          backgroundSize: "cover",
-          backgroundPosition: "50% 50%",
+          backgroundImage: `url(${imgUrl}`,
         }}
       ></div>
     </div>
--- pages/_document.js
+++ pages/_document.js
@@ -11,7 +11,7 @@ export default function Document() {
           crossOrigin="true"
         />
         <link
-          href="https://fonts.googleapis.com/css2?family=Roboto+Slab&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@700&display=swap"
           rel="stylesheet"
         />
       </Head>
