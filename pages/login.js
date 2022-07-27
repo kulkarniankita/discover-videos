6eaeeaf481aa393684590ca31075818a93ee8faf
ankita kulkarni <kulkarni.ankita09@gmail.com>
2022-07-27 14:20:39 -0400

    Sign-in Page Structure

--- pages/login.js
+++ pages/login.js
@@ -2,9 +2,14 @@ import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
 
-import styles from "../styles/";
+import styles from "../styles/Login.module.css";
 
 const Login = () => {
+  const handleLoginWithEmail = async (e) => {
+    e.preventDefault();
+    console.log("hi button");
+  };
+
   return (
     <div>
       <Head>
@@ -27,6 +32,23 @@ const Login = () => {
           </Link>
         </div>
       </header>
+
+      <main className={styles.main}>
+        <div className={styles.mainWrapper}>
+          <h1 className={styles.signinHeader}>Sign In</h1>
+
+          <input
+            type="text"
+            placeholder="Email address"
+            className={styles.emailInput}
+          />
+
+          <p className={styles.userMsg}></p>
+          <button onClick={handleLoginWithEmail} className={styles.loginBtn}>
+            Sign In
+          </button>
+        </div>
+      </main>
     </div>
   );
 };
