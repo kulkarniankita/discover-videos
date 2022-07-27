c7c8c71d67ea7ddc95c369e1bc59de20097480f9
ankita kulkarni <kulkarni.ankita09@gmail.com>
2022-07-27 16:30:05 -0400

    App Route once LoggedIn

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,26 @@
-import '../styles/globals.css'
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import { magic } from "../lib/magic-client";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleLoggedIn = async () => {
+      const isLoggedIn = await magic.user.isLoggedIn();
+      if (isLoggedIn) {
+        // route to /
+        router.push("/");
+      } else {
+        // route to /login
+        router.push("/login");
+      }
+    };
+    handleLoggedIn();
+  }, []);
+
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
