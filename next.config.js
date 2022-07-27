3891bd2c0477a165004b2ada163c176b050c7358
ankita kulkarni <kulkarni.ankita09@gmail.com>
2022-07-27 12:07:18 -0400

    Implement Youtube Search API (continued)

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ["images.unsplash.com"],
+    domains: ["images.unsplash.com", "i.ytimg.com"],
   },
 };
 
