5369b077efdcac54f95a7905bc3ab6e3eb716a97
ankita kulkarni <kulkarni.ankita09@gmail.com>
2022-07-27 14:03:53 -0400

    Sign-in Header Component

--- pages/login.js
+++ pages/login.js
@@ -1,4 +1,8 @@
 import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+
+import styles from "../styles/";
 
 const Login = () => {
   return (
@@ -6,6 +10,23 @@ const Login = () => {
       <Head>
         <title>Netflix SignIn</title>
       </Head>
+
+      <header>
+        <div className={styles.headerWrapper}>
+          <Link className={styles.logoLink} href="/">
+            <a>
+              <div className={styles.logoWrapper}>
+                <Image
+                  src="/static/netflix.svg"
+                  alt="Netflix logo"
+                  width="128px"
+                  height="34px"
+                />
+              </div>
+            </a>
+          </Link>
+        </div>
+      </header>
     </div>
   );
 };
