7f53d360a224d26eca4a7f94ae29b9570c91992f
ankita kulkarni <kulkarni.ankita09@gmail.com>
2022-07-27 14:37:42 -0400

    Sign-in Form Validation

--- pages/login.js
+++ pages/login.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
@@ -5,9 +6,25 @@ import Link from "next/link";
 import styles from "../styles/Login.module.css";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [userMsg, setUserMsg] = useState("");
+  const handleOnChangeEmail = (e) => {
+    setUserMsg("");
+    console.log("event", e);
+    const email = e.target.value;
+    setEmail(email);
+  };
+
   const handleLoginWithEmail = async (e) => {
-    e.preventDefault();
     console.log("hi button");
+    e.preventDefault();
+
+    if (email) {
+      // route to dashboard
+    } else {
+      // show user message
+      setUserMsg("Enter a valid email address");
+    }
   };
 
   return (
@@ -41,9 +58,10 @@ const Login = () => {
             type="text"
             placeholder="Email address"
             className={styles.emailInput}
+            onChange={handleOnChangeEmail}
           />
 
-          <p className={styles.userMsg}></p>
+          <p className={styles.userMsg}>{userMsg}</p>
           <button onClick={handleLoginWithEmail} className={styles.loginBtn}>
             Sign In
           </button>
