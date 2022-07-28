c36c6fda4647dee9ce56aa228a05d57729c2f16d
ankita kulkarni <kulkarni.ankita09@gmail.com>
2022-07-27 23:03:43 -0400

    Connect Banner Component

--- components/banner/banner.js
+++ components/banner/banner.js
@@ -1,11 +1,14 @@
 import Image from "next/image";
+import { useRouter } from "next/router";
 import styles from "./banner.module.css";
 
 const Banner = (props) => {
-  const { title, subTitle, imgUrl } = props;
+  const { title, subTitle, imgUrl, videoId } = props;
+  const router = useRouter();
 
   const handleOnPlay = () => {
     console.log("handleOnPlay");
+    router.push(`video/${videoId}`);
   };
 
   return (
--- pages/index.js
+++ pages/index.js
@@ -39,6 +39,7 @@ export default function Home({
       <div className={styles.main}>
         <NavBar />
         <Banner
+          videoId="4zH5iYM4wJo"
           title="Clifford the red dog"
           subTitle="a very cute dog"
           imgUrl="/static/clifford.webp"
