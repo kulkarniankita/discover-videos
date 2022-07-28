4a8a15b6ac339ba5bfa50bfa0a872d0701f65d03
ankita kulkarni <kulkarni.ankita09@gmail.com>
2022-07-28 00:15:14 -0400

    YouTube Quota Limit: Test Data

--- pages/video/[videoId].js
+++ pages/video/[videoId].js
@@ -9,17 +9,6 @@ import { getYoutubeVideoById } from "../../lib/videos";
 Modal.setAppElement("#__next");
 
 export async function getStaticProps(context) {
-  //data to fetch from API
-  // const video = {
-  //   title: "Hi cute dog",
-  //   publishTime: "1990-01-01",
-  //   description:
-  //     "A big red dog that is super cute, can he get any bigger? A big red dog that is super cute, can he get any bigger?",
-  //   channelTitle: "Paramount Pictures",
-  //   viewCount: 10000,
-  // };
-  console.log({ context });
-
   const videoId = context.params.videoId;
 
   const videoArray = await getYoutubeVideoById(videoId);
