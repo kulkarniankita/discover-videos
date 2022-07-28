c08776c84ead621ae6102c61b171e1acba622dbf
ankita kulkarni <kulkarni.ankita09@gmail.com>
2022-07-28 14:21:26 -0400

    Build Stats with Request Cookies

--- pages/api/stats.js
+++ pages/api/stats.js
@@ -1,5 +1,16 @@
 export default async function stats(req, resp) {
   if (req.method === "POST") {
-    resp.send({ msg: "it works" });
+    console.log({ cookies: req.cookies });
+
+    try {
+      if (!req.cookies.token) {
+        resp.status(403).send({});
+      } else {
+        resp.send({ msg: "it works" });
+      }
+    } catch (error) {
+      console.error("Error occurred /stats", error);
+      resp.status(500).send({ done: false, error: error?.message });
+    }
   }
 }
