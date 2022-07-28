207531829f4396210cca402f99d6cf6c853cda86
ankita kulkarni <kulkarni.ankita09@gmail.com>
2022-07-28 11:12:27 -0400

    Login API Create Auth Header

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,6 +1,9 @@
 export default async function login(req, res) {
   if (req.method === "POST") {
     try {
+      const auth = req.headers.authorization;
+      const didToken = auth ? auth.substr(7) : "";
+      console.log({ token });
       res.send({ done: true });
     } catch (error) {
       console.error("Something went wrong logging in", error);
