1c56504cfa1d0b1c45c12b835a5fbcde7b5d791e
ankita kulkarni <kulkarni.ankita09@gmail.com>
2022-07-28 11:09:11 -0400

    Login API

---
+++ pages/api/login.js
@@ -0,0 +1,12 @@
+export default async function login(req, res) {
+  if (req.method === "POST") {
+    try {
+      res.send({ done: true });
+    } catch (error) {
+      console.error("Something went wrong logging in", error);
+      res.status(500).send({ done: false });
+    }
+  } else {
+    res.send({ done: false });
+  }
+}
