f4a30dbbd84dcef06cf19ecd2aa8934cbd5c60ec
ankita kulkarni <kulkarni.ankita09@gmail.com>
2022-07-28 13:59:44 -0400

    Like and Dislike Button Functionality

--- pages/video/[videoId].js
+++ pages/video/[videoId].js
@@ -4,7 +4,11 @@ import styles from "../../styles/Video.module.css";
 
 import NavBar from "../../components/nav/navbar";
 import clsx from "classnames";
+
 import { getYoutubeVideoById } from "../../lib/videos";
+
+import Like from "../../components/icons/like-icon";
+import DisLike from "../../components/icons/dislike-icon";
 
 Modal.setAppElement("#__next");
 
@@ -61,6 +65,19 @@ const Video = ({ video }) => {
           src={`https://www.youtube.com/embed/${router.query.videoId}?autoplay=0&origin=http://example.com&controls=0&rel=1`}
           frameBorder="0"
         ></iframe>
+
+        <div className={styles.likeDislikeBtnWrapper}>
+          <div className={styles.btnWrapper}>
+            <button>
+              <Like />
+            </button>
+          </div>
+          <div className={styles.btnWrapper}>
+            <button>
+              <DisLike />
+            </button>
+          </div>
+        </div>
 
         <div className={styles.modalBody}>
           <div className={styles.modalBodyContent}>
