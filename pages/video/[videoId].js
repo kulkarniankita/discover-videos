6cc6ae9cd200ff431627d0f44d10e95653d35c63
ankita kulkarni <kulkarni.ankita09@gmail.com>
2022-07-27 23:29:37 -0400

    Modal Component Structure

--- pages/video/[videoId].js
+++ pages/video/[videoId].js
@@ -1,12 +1,24 @@
 import { useRouter } from "next/router";
 import Modal from "react-modal";
 import styles from "../../styles/Video.module.css";
+import clsx from "classnames";
 
 Modal.setAppElement("#__next");
 
 const Video = () => {
   const router = useRouter();
   console.log({ router });
+
+  const video = {
+    title: "Hi cute dog",
+    publishTime: "1990-01-01",
+    description: "A big red dog that is super cute, can he get any bigger?",
+    channelTitle: "Paramount Pictures",
+    viewCount: 10000,
+  };
+
+  const { title, publishTime, description, channelTitle, viewCount } = video;
+
   return (
     <div className={styles.container}>
       <Modal
@@ -25,6 +37,26 @@ const Video = () => {
           src={`https://www.youtube.com/embed/${router.query.videoId}?autoplay=0&origin=http://example.com&controls=0&rel=1`}
           frameBorder="0"
         ></iframe>
+
+        <div className={styles.modalBody}>
+          <div className={styles.modalBodyContent}>
+            <div className={styles.col1}>
+              <p className={styles.publishTime}>{publishTime}</p>
+              <p className={styles.title}>{title}</p>
+              <p className={styles.description}>{description}</p>
+            </div>
+            <div className={styles.col2}>
+              <p className={clsx(styles.subText, styles.subTextWrapper)}>
+                <span className={styles.textColor}>Cast: </span>
+                <span className={styles.channelTitle}>{channelTitle}</span>
+              </p>
+              <p className={clsx(styles.subText, styles.subTextWrapper)}>
+                <span className={styles.textColor}>View Count: </span>
+                <span className={styles.channelTitle}>{viewCount}</span>
+              </p>
+            </div>
+          </div>
+        </div>
       </Modal>
     </div>
   );
