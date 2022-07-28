940850b4ae8524c4ec0a94329e5e8a408718b3e1
ankita kulkarni <kulkarni.ankita09@gmail.com>
2022-07-27 23:17:53 -0400

    Implement Youtube API Player & Docs

--- pages/video/[videoId].js
+++ pages/video/[videoId].js
@@ -16,6 +16,14 @@ const Video = () => {
         className={styles.modal}
         overlayClassName={styles.overlay}
       >
+        <iframe
+          id="ytplayer"
+          type="text/html"
+          width="100%"
+          height="360"
+          src={`https://www.youtube.com/embed/${router.query.videoId}?autoplay=0&origin=http://example.com&controls=0&rel=1`}
+          frameBorder="0"
+        ></iframe>
         <div>Modal body</div>
       </Modal>
     </div>
