0a11a44d9ff3d66fd835901d519a49633ae7dc46
ankita kulkarni <kulkarni.ankita09@gmail.com>
2022-07-28 12:03:43 -0400

    Login API Create JWT (JSON Web Token)

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,16 +1,33 @@
 import { magicAdmin } from "../../lib/magic";
+import jwt from "jsonwebtoken";
 
 export default async function login(req, res) {
   if (req.method === "POST") {
     try {
       const auth = req.headers.authorization;
       const didToken = auth ? auth.substr(7) : "";
-      console.log({ didToken });
+      // invoke magic
 
       const metadata = await magicAdmin.users.getMetadataByToken(didToken);
       console.log({ metadata });
 
-      //invoke magic
+      // create jwt
+
+      const token = jwt.sign(
+        {
+          ...metadata,
+          iat: Math.floor(Date.now() / 1000),
+          exp: Math.floor(Date.now() / 1000 + 7 * 24 * 60 * 60),
+          "https://hasura.io/jwt/claims": {
+            "x-hasura-allowed-roles": ["user", "admin"],
+            "x-hasura-default-role": "user",
+            "x-hasura-user-id": `${metadata.issuer}`,
+          },
+        },
+        "thisisasecretthisisasecrett1234"
+      );
+      console.log({ token });
+
       res.send({ done: true });
     } catch (error) {
       console.error("Something went wrong logging in", error);
