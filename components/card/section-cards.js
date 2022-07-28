f42154a0a7b3a861ea09670ffb82b19ddfa62233
ankita kulkarni <kulkarni.ankita09@gmail.com>
2022-07-27 22:58:43 -0400

    Solution: Link Component

--- components/card/section-cards.js
+++ components/card/section-cards.js
@@ -12,7 +12,7 @@ const SectionCards = (props) => {
         {videos.map((video, idx) => {
           console.log({ video });
           return (
-            <Link key={idx} href={`/video/${videoId}`}>
+            <Link key={idx} href={`/video/${video.id}`}>
               <Card key={idx} id={idx} imgUrl={video.imgUrl} size={size} />
             </Link>
           );
