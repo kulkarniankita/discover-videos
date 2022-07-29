8fffb75cdf4f278ec9b2e0258a890b4a3fe683ec
ankita kulkarni <kulkarni.ankita09@gmail.com>
2022-07-28 23:03:54 -0400

    Logout Solution

--- components/nav/navbar.js
+++ components/nav/navbar.js
@@ -1,14 +1,16 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 import styles from "./navbar.module.css";
 
 import { useRouter } from "next/router";
 import Link from "next/link";
 import Image from "next/image";
+
 import { magic } from "../../lib/magic-client";
 
 const NavBar = () => {
   const [showDropdown, setShowDropdown] = useState(false);
   const [username, setUsername] = useState("");
+  const [didToken, setDidToken] = useState("");
   const router = useRouter();
 
   useEffect(() => {
@@ -46,9 +48,15 @@ const NavBar = () => {
     e.preventDefault();
 
     try {
-      await magic.user.logout();
-      console.log(await magic.user.isLoggedIn());
-      router.push("/login");
+      const response = await fetch("/api/logout", {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${didToken}`,
+          "Content-Type": "application/json",
+        },
+      });
+
+      const res = await response.json();
     } catch (error) {
       console.error("Error logging out", error);
       router.push("/login");
@@ -58,16 +66,18 @@ const NavBar = () => {
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
-        <a className={styles.logoLink}>
-          <div className={styles.logoWrapper}>
-            <Image
-              src="/static/netflix.svg"
-              alt="Netflix logo"
-              width="128px"
-              height="34px"
-            />
-          </div>
-        </a>
+        <Link className={styles.logoLink} href="/">
+          <a>
+            <div className={styles.logoWrapper}>
+              <Image
+                src="/static/netflix.svg"
+                alt="Netflix logo"
+                width="128px"
+                height="34px"
+              />
+            </div>
+          </a>
+        </Link>
 
         <ul className={styles.navItems}>
           <li className={styles.navItem} onClick={handleOnClickHome}>
@@ -81,13 +91,15 @@ const NavBar = () => {
           <div>
             <button className={styles.usernameBtn} onClick={handleShowDropdown}>
               <p className={styles.username}>{username}</p>
+              {/** Expand more icon */}
               <Image
-                src="/static/expand_more.svg"
-                alt="Expand more"
+                src={"/static/expand_more.svg"}
+                alt="Expand dropdown"
                 width="24px"
                 height="24px"
               />
             </button>
+
             {showDropdown && (
               <div className={styles.navDropdown}>
                 <div>
---
+++ pages/api/logout.js
@@ -0,0 +1,26 @@
+import { magicAdmin } from "../../lib/magic";
+import { removeTokenCookie } from "../../lib/cookies";
+import { verifyToken } from "../../lib/utils";
+
+export default async function logout(req, res) {
+  try {
+    if (!req.cookies.token)
+      return res.status(401).json({ message: "User is not logged in" });
+    const token = req.cookies.token;
+
+    const userId = await verifyToken(token);
+    removeTokenCookie(res);
+    try {
+      await magicAdmin.users.logoutByIssuer(userId);
+    } catch (error) {
+      console.log("User's session with Magic already expired");
+      console.error("Error occurred while logging out magic user", error);
+    }
+    //redirects user to login page
+    res.writeHead(302, { Location: "/login" });
+    res.end();
+  } catch (error) {
+    console.error({ error });
+    res.status(401).json({ message: "User is not logged in" });
+  }
+}
