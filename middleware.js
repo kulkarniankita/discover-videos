9fbcbe0685075be1705be3169bbb11a34ab39ec9
ankita kulkarni <kulkarni.ankita09@gmail.com>
2022-07-28 23:47:33 -0400

    NextJS 12: Middleware Implementation Continued

---
+++ middleware.js
@@ -0,0 +1,22 @@
+import { NextResponse } from "next/server";
+import { verifyToken } from "./lib/utils";
+
+export async function middleware(req, ev) {
+  const token = req ? req.cookies?.token : null;
+  const userId = await verifyToken(token);
+  const { pathname } = req.nextUrl;
+
+  if (
+    pathname.includes("/api/login") ||
+    userId ||
+    pathname.includes("/static")
+  ) {
+    return NextResponse.next();
+  }
+
+  if ((!token || !userId) && pathname !== "/login") {
+    const url = req.nextUrl.clone();
+    url.pathname = "/login";
+    return NextResponse.rewrite(url);
+  }
+}
