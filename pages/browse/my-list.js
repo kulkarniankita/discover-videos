d395b8b75a2d903f05bd6b806f044d6fdc2ac43c
ankita kulkarni <kulkarni.ankita09@gmail.com>
2022-07-28 22:57:10 -0400

    Server Side Render My List Page

--- pages/browse/my-list.js
+++ pages/browse/my-list.js
@@ -2,9 +2,22 @@ import Head from "next/head";
 import NavBar from "../../components/nav/navbar";
 
 import SectionCards from "../../components/card/section-cards";
+import { redirectUser } from "../../utils/redirectUser";
+import { getMyList } from "../../lib/videos";
 import styles from "../../styles/MyList.module.css";
 
-const MyList = () => {
+export async function getServerSideProps(context) {
+  const { userId, token } = await redirectUser(context);
+  const videos = await getMyList(userId, token);
+
+  return {
+    props: {
+      myListVideos: videos,
+    },
+  };
+}
+
+const MyList = ({ myListVideos }) => {
   return (
     <div>
       <Head>
@@ -13,7 +26,7 @@ const MyList = () => {
       <main className={styles.main}>
         <NavBar />
         <div className={styles.sectionWrapper}>
-          <SectionCards title="My List" videos={[]} size="small" />
+          <SectionCards title="My List" videos={myListVideos} size="small" />
         </div>
       </main>
     </div>
